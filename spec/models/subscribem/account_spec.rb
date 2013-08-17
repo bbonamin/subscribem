42d867449b641eeac55082ed830f1da079a89b02
Bruno Bonamin <bruno@bonamin.org>
2013-08-17 19:03:43 -0300

    root url errors, when specs should be passing (Page 87)

--- spec/models/subscribem/account_spec.rb
+++ spec/models/subscribem/account_spec.rb
@@ -22,5 +22,22 @@ module Subscribem
       account.should_not be_valid
       account.users.should be_empty
     end
+
+    def schema_exists?(account)
+      query = %Q{ SELECT nspname FROM pg_namespace
+                  WHERE nspname='#{account.subdomain}'}
+      result = ActiveRecord::Base.connection.select_value(query)
+      result.present?
+    end
+
+    it 'creates a schema' do
+      account = Subscribem::Account.create!({
+        name: 'First Account',
+        subdomain: 'first'
+        })
+      account.create_schema
+      failure_message = "Schema #{account.subdomain} does not exist"
+      assert schema_exists?(account), failure_message
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'factory_girl'
+require 'database_cleaner'
 require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -27,10 +28,31 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  config.before(:all) do
+    DatabaseCleaner.strategy = :truncation,
+    {:pre_count => true, :reset_ids => true}
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  
+  config.after(:each) do
+    Apartment::Database.reset
+    DatabaseCleaner.clean
+    connection = ActiveRecord::Base.connection.raw_connection
+    schemas = connection.query(%Q{
+      SELECT 'drop schema ' || nspname || ' cascade;'
+      from pg_namespace
+      where nspname != 'public'
+      AND nspname NOT LIKE 'pg_%'
+      AND nspname != 'information_schema';
+      })
+    schemas.each do |query|
+      connection.query(query.values.first)
+    end
+  end
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -42,4 +64,5 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
 end
